36db5373b902776a14ffa2af4b54212f25a2648e
Brian Malone <36305546+Bmalone100@users.noreply.github.com>
2024-08-16 16:29:55 +0100

    changes 16th aug

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-classes',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatTableModule, MatListModule, MatIcon, FlexLayoutModule],
   templateUrl: './classes.component.html',
-  styleUrl: './classes.component.scss'
+  styleUrls: ['./classes.component.scss']
 })
 export class ClassesComponent {
+  // Define the data sources for the tables
+  tuesdayData = [
+    { time: '7:00 PM - 7:45 PM', classType: '6 to 8 year olds' },
+    { time: '7:45 PM - 8:30 PM', classType: 'under 13s' },
+    { time: '8:30 PM - 9:15 PM', classType: 'Over 13s and Seniors' }
+  ];
 
+  fridayData = [
+    { time: '7:45 PM - 8:30 PM', classType: 'under 13s' },
+    { time: '8:30 PM - 9:15 PM', classType: 'Over 13s and Seniors' }
+  ];
+
+  saturdayData = [
+    { time: '10:00 AM - 11:00 AM', classType: '4-6 year olds' }
+  ];
+
+  displayedColumns: string[] = ['time', 'classType'];
 }
