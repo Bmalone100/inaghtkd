092115eb8441e034d805349ab24fa3b7257a8ba5
Bmalone100 <brian.malone_social@outlook.ie>
2024-09-05 10:29:47 +0100

    cleaning main branch for local dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ClassesComponent } from './classes/classes.component';
-import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'classes', component: ClassesComponent},
-    { path: 'admin', component: AdminComponent}
+    { path: 'classes', component: ClassesComponent}
   ];
   
